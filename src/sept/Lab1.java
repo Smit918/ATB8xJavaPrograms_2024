51817b0c3fb37ccdbc3db5602638d177f9bdbdaa
Smit <smit.trivedi1929@gmail.com>
2024-09-14 13:27:28 +0530

    Excercise for the 13th Sept 2024

--- src/sept/Lab1.java
+++ src/sept/Lab1.java
@@ -1,4 +1,24 @@
 package sept;
 
+
 public class Lab1 {
+    public static void main(String[] args) {
+        double a = 34;
+        double b=10;
+
+        double add = a+b;
+        double sub = a-b;
+        double mul = a-b;
+        double div = a/b;
+        double mod = a%b;
+
+
+        System.out.println("The value of addition is :"+add);
+        System.out.println("The value of subtration is :"+sub);
+        System.out.println("The value of multiplication is :"+mul);
+        System.out.println("The value of division is :"+div);
+        System.out.println("The value of modules is :"+mod);
+
+
+    }
 }
